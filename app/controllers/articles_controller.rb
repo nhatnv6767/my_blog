b1f764e79064596cb1c4f1f648066faf42a8c15a
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-02-02 01:48:43 +0900

    show articles route, action and view, use byebug to debug

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+
+  def show
+    # tương tự như trong console ta làm là article = Article.find(1) or (2)
+    # sau đó muốn truy cập đến các phần tử article.title or article.description
+    # this is only availble for this method show,
+    # Để làm cho biến này có sẵn cho bên phần view hoặc các phần khác, ta phải biến nó trở thành
+    # một instance variable để có thể sử dụng ở nhiều chỗ -> thêm @ vào trước article
+    # @ biến 1 biến bình thường trở thành biến instance
+    # byebug: use to debug
+    byebug
+    @article = Article.find(params[:id])
+  end
+
+end
