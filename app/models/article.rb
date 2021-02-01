cb6eb4885dcf65f1d31b3108ac10b71a5a634c39
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-02-02 01:10:18 +0900

    validate and make some route for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-  
+  # validate giá trị title, presence nghĩa là tiêu đề có sẵn có bất kỳ Article, nghĩa là
+  # từ giờ nếu title = nil sẽ không thể thực hiện lệnh save được nữa
+  validates :title, presence: true, length: { minimum: 6, maximum: 100}
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
