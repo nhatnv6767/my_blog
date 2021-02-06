2b2b03b552caae6ce0471011e73b42d568cbcb77
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-02-06 10:32:22 +0900

    messaging validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,10 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    # Để khởi tạo giá trị đầu tiên cho instance @article
+    # Nếu không khởi tạo, khi vào trang /new kết hợp với code check đầu ở new.html.erb sẽ gây lỗi
+    # vì chưa có instance @article mà lại đi check nó, -> wrong
+    @article = Article.new
 
   end
 
@@ -25,13 +29,21 @@ class ArticlesController < ApplicationController
     @article = Article.new(params.require(:article).permit(:title, :description))
     # hien thi thong tin chi tiet ra
     # render plain: @article.inspect
-    @article.save
-    # ở rails routes --expanded ta thấy Prefix article, đó là lý do vì sao có article_path
-    # nó sẽ trích xuất id từ @article, và sau đó sử dụng nó để tạo đường dẫn
-    # thường được sử dụng nếu ta chỉ chuyển hướng đơn giản là tại article ngay tại đây
-    # redirect_to article_path(@article)
-    # nên để viết ngắn hơn ta có thể dùng như dưới
-    redirect_to @article
+    if @article.save
+      # flash for message success
+      flash[:notice] = "Article was created successfully."
+      # ở rails routes --expanded ta thấy Prefix article, đó là lý do vì sao có article_path
+      # nó sẽ trích xuất id từ @article, và sau đó sử dụng nó để tạo đường dẫn
+      # thường được sử dụng nếu ta chỉ chuyển hướng đơn giản là tại article ngay tại đây
+      # redirect_to article_path(@article)
+      # nên để viết ngắn hơn ta có thể dùng như dưới
+      redirect_to @article
+
+    else
+      # Khi lưu không thành công sẽ trở lại trang new để thêm lại mẫu
+      # và các thông báo sẽ chuyển đến đây, thông báo lỗi
+      render 'new'
+    end
   end
 
 end
