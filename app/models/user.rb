fb94a320dffb9eaec747bc10a7efd948473465f4
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-03-06 11:49:14 +0900

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  # true: đảm bảo tên người dùng được presence trước khi một đối tượng người dùng được lưu vào CSDL
+  # uniqueness check nếu như trùng tên username bao gồm cả viết hoa và viết thường
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX}
 end
