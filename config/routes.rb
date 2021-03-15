89a6dbf14eb2e29ee4c2777d5242b483d50161b5
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-03-16 01:42:08 +0900

    new user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # thêm các routes cho articles
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  # <=>
+  resources :users, except: [:new]
 end
