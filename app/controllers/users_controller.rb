c3aaccd49b998efb79c36fb29446283b0a6dc642
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-03-17 01:58:14 +0900

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    # Đưa các thông số vào danh sách trắng
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  # tương tự như ở articles_controller
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
